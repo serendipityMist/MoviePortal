0e0122a135379a1ccb49fbef7af4679a30e62cb0
serendipityMist <serendipityMist@gmail.com>
2024-12-10 07:37:44 +0545

    make change

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ const Header = () => {
   return (
     <>
       <div className="w-full bg-blue-800 p-3">
-        <h1 className="text-3xl text-white bold">MistMovies</h1>
+        <h1 className="text-3xl text-white font-bold">MistMovies</h1>
       </div>
     </>
   );
--- src/components/MoviePortal.jsx
+++ src/components/MoviePortal.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useRef, useState } from "react";
 import { fetchMovies } from "../api/FetchMovies";
 import Card from "./Card";
 
@@ -7,11 +7,13 @@ const MoviePortal = () => {
   const [message, setMessage] = useState([]);
   const [errMessage, setErrMessage] = useState(null);
 
+  const inputRef = useRef();
+
   const onSearchTextEnter = (e) => {
     e.preventDefault();
     fetchMovies(searchMovie, setMessage, setErrMessage, () => {});
-    setSearchMovie("");
-    // inputRef.current.value = "";
+    // setSearchMovie("");
+    inputRef.current.value = "";
   };
 
   return (
@@ -21,9 +23,9 @@ const MoviePortal = () => {
         className="  w-1/2 flex justify-center m-auto h-full"
       >
         <input
+          ref={inputRef}
           type="text"
           placeholder="Search Movie"
-          value={searchMovie}
           onChange={(e) => {
             setSearchMovie(e.target.value);
           }}
@@ -31,19 +33,21 @@ const MoviePortal = () => {
         />
       </form>
       {errMessage && <p style={{ color: "red" }}>{errMessage}</p>}
-
+      <h1 className="font-bold text-lg">
+        Current Searched Movie: {searchMovie}{" "}
+      </h1>
       <div className="border w-full flex flex-wrap justify-center gap-2 p-1  bg-white">
         {Array.isArray(message) && message.length > 0 ? (
           message.map((element, index) => (
             <div
               key={index}
-              className="text-white border p-1 rounded-lg bg-black w-full  md:w-1/3 lg:w-1/4  flex-grow  lg:max-w-[300px]"
+              className="text-white border p-1 rounded-lg bg-black w-full  md:w-1/3 lg:w-1/4  flex-grow  lg:max-w-[300px]  drop-shadow-sm"
             >
               <Card pass={element} />
             </div>
           ))
         ) : (
-          <p>No result</p>
+          <p className="font-bold text-xl">No result found!</p>
         )}
       </div>
     </>
